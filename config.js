4b15a543c4046964776cb3c3878d924ae6b72058
vutran030 <90592032+vutran030@users.noreply.github.com>
2021-09-13 11:21:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Lan à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Lan cứ đi nhầm đường
+    Lạc vào tim Vũ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Phải chăng Vũ đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Vũ đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Vũ <33',
+    btnNo: 'Không, B vũ à:< :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Lan lại yêu Vũ <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'khi nào được gặp sẽ biết lý do thôiiii <33333333',
+    mess: 'Vũ biết mà 🥰. I love you chucamo 😘😘',
+    messDesc: 'khi nào gặp thì Vũ mời đi ăn nhé:333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
